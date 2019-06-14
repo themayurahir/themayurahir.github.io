3039ebc70958c26a84367adc4a912c959ea02183
Mayur ahir <mayurahir33@gmail.com>
2019-06-14 10:49:11 +0530

    test2

--- testing/test2.js
+++ testing/test2.js
@@ -57,3 +57,60 @@
   }).call(this);
   
   //# sourceURL=coffeescript
+
+
+
+  // Loaders JavaScript
+
+  (function () {
+    function fullSketch() {
+      var begin = 0;
+      var durations = [2,2,1];
+      var pathsNode = document.querySelectorAll('.main');
+      var paths = Array.from(pathsNode);
+      var length = 0;
+
+      // paths[0].getBoundingClientRect();
+
+      paths.forEach(function(path, i) {
+        // console.log(path);
+        length = path.getTotalLength();
+        // Clear any style
+        path.style.transition = path.style.WebkitTransition = 'none';
+        path.style.strokeDasharray = length + ' ' + length;
+        path.style.strokeDashoffset = length;
+        // Trigger
+        path.getBoundingClientRect();
+        path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset ' + durations[i] + 's ' + begin + 's ease';
+        path.style.strokeDashoffset = '0';
+        begin += durations[i];
+
+        setTimeout(addLightningAnimate, 5000);
+        setTimeout(removeLightningAnimate, 8000);
+      });
+
+      function addLightningAnimate() {
+        var lightning = document.getElementById("lightning");
+        lightning.classList.add("lightningAnimate");
+      }
+
+      function removeLightningAnimate() {
+        var lightning = document.getElementById("lightning");
+        lightning.classList.remove("lightningAnimate");
+      }
+      //setTimeout(addLightningAnimate, 5000);
+      setTimeout(fullSketch, 7000);
+      setTimeout(showPage, 20000);
+    }
+
+    function showPage() {
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("myImgs").style.display = "block";
+      }
+  
+    function alert() {
+      console.log("it loaded!");
+    }
+
+    window.addEventListener('load', fullSketch);
+  }());
